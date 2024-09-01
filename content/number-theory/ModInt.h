856f22522d542cb827164b307fe38a93196319ee
Adam Soltan <adam.henryk.soltan@gmail.com>
2024-09-01 20:32:15 +0200

    Fix error in ModInt

--- content/number-theory/ModInt.h
+++ content/number-theory/ModInt.h
@@ -9,16 +9,16 @@ struct Mod {
   static const int MOD = M, ROOT = R;
   int x;
   Mod(ll y = 0) : x(y % M) { x += (x < 0) * M; }
-  Mod operator+=(Mod o) {
+  Mod& operator+=(Mod o) {
     if ((x += o.x) >= M) x -= M;
     return *this; }
-  Mod operator-=(Mod o) {
+  Mod& operator-=(Mod o) {
     if ((x -= o.x) < 0) x += M;
     return *this; }
-  Mod operator*=(Mod o) {
+  Mod& operator*=(Mod o) {
     x = 1ll * x * o.x % M;
     return *this; }
-  Mod operator/=(Mod o) { return (*this) *= o.inv(); }
+  Mod& operator/=(Mod o) { return *this *= o.inv(); }
   friend Mod operator+(Mod a, Mod b) { return a += b; }
   friend Mod operator-(Mod a, Mod b) { return a -= b; }
   friend Mod operator*(Mod a, Mod b) { return a *= b; }
