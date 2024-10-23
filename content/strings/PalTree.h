0eed5fe179d0498561b484d70f1bf366b14d6771
Adam Soltan <adam.henryk.soltan@gmail.com>
2024-10-23 21:09:20 +0200

    Min palindromic split dp

--- content/strings/PalTree.h
+++ content/strings/PalTree.h
@@ -1,31 +1,52 @@
 /**
  * Author: UW1, Adam Soltan
- * Description: Palindrome tree. To count occurrences of each palindrome,
- * after every \texttt{add} add $1$ to vertices on the suffix link path
- * from \texttt{last} to the root.
- * Time: O(26 N)
+ * Description: Palindrome tree. Can be used for counting number of occurrences,
+ * just add $1$ to suffix link path. Replace array with map if ML is tight.
+ * Time: O(26 N), \texttt{split} is O(n \log n)
+ * Status: split tested on https://acm.timus.ru/problem.aspx?space=1&num=2058
  */
 #pragma once
 
-template<int A> struct PalTree {
-  struct Node { int len = 0, next[A] = {}, link = 0; };
-  vector<Node> t;
-  vi s = {-1};
+const int A = 26;
+struct PalTree {
   int last = 0;
-  PalTree() : t(2) { t[0].link = 1; t[1].len = -1; }
+  vi len = {0, -1}, link = {1, 0}, s = {-1};
+  vector<array<int, A>> to = {{}, {}};
   int find(int u) {
-    while (s.back() != s[sz(s) - t[u].len - 2]) u = t[u].link;
+    while (s.back() != s[sz(s) - len[u] - 2]) u = link[u];
     return u;
   }
-  int add(int c) {
-    s.push_back(c);
-    last = find(last);
-    if (!t[last].next[c]) {
-      Node& n = t.emplace_back();
-      n.len = t[last].len + 2;
-      n.link = t[find(t[last].link)].next[c];
-      t[last].next[c] = sz(t) - 1;
+  int add(int x) { // x in [0, A)
+    s.push_back(x); last = find(last);
+    if (!to[last][x]) {
+      to.push_back({});
+      len.push_back(len[last] + 2);
+      link.push_back(to[find(link[last])][x]);
+      to[last][x] = sz(to) - 1;
     }
-    return last = t[last].next[c];
+    return last = to[last][x];
   }
 };
+// dp[i] = min even/odd palindromic split of prefix of size i
+const int INF = 1e9;
+struct F { int e, o; };
+F op(F x, F y) { return {min(x.e, y.e), min(x.o, y.o)}; }
+vector<F> split(vi v) {
+  PalTree t;
+  vector<F> s(2), ans(sz(v) + 1, {INF, INF});
+  vi go(2), d(2); ans[0] = s[0] = s[1] = {0, INF};
+  rep(i, 0, sz(v)) {
+    int x = t.add(v[i]), y = t.link[x];
+    if (x >= sz(go)) {
+      d.push_back(t.len[x] - t.len[y]);
+      go.push_back(d[x] == d[y] ? go[y] : y);
+      s.push_back(ans[0]);
+    }
+    for (int u = x; t.len[u] > 0; u = go[u]) {
+      s[u] = ans[i + 1 - t.len[go[u]] - d[u]];
+      if (d[u] == d[t.link[u]]) s[u] = op(s[u], s[t.link[u]]);
+      ans[i + 1] = op(ans[i + 1], {s[u].o + 1, s[u].e + 1});
+    }
+  }
+  return ans;
+}
--- tests/strings/PalTree.test.cpp
+++ tests/strings/PalTree.test.cpp
@@ -10,16 +10,15 @@ using pii = pair<int, int>;
 using vi = vector<int>;
 
 #ifdef LOCAL
-auto& operator<<(auto&, pair<auto, auto>);
+auto operator<<(auto& o, auto x) -> decltype(x.first, o);
 auto operator<<(auto& o, auto x) -> decltype(x.end(), o) {
   o << "{";
   for (int i = 0; auto y : x) o << ", " + !i++ * 2 << y;
-  return o << "}";
-}
-auto& operator<<(auto& o, pair<auto, auto> x) {
+  return o << "}"; }
+auto operator<<(auto& o, auto x) -> decltype(x.first, o) {
   return o << "(" << x.first << ", " << x.second << ")"; }
-void __print(auto... x) { ((cerr << " " << x), ...) << endl; }
-#define debug(x...) cerr << "[" #x "]:", __print(x)
+void __print(auto... x) { ((cerr << x << " "), ...) << endl; }
+#define debug(x...) __print("[" #x "]:", x)
 #else
 #define debug(...) 2137
 #endif
@@ -30,13 +29,13 @@ int main() {
   cin.tie(0)->sync_with_stdio(0);
   string s;
   cin >> s;
-  PalTree<26> t;
+  PalTree t;
   vi v(sz(s));
   rep(i, 0, sz(s)) v[i] = t.add(s[i] - 'a');
-  vi p(sz(t.t));
-  rep(i, 0, sz(p)) for (int j : t.t[i].next) if (j) p[j] = i;
+  vi p(sz(t.len));
+  rep(i, 0, sz(p)) for (int j : t.to[i]) if (j) p[j] = i;
   cout << sz(p) - 2 << '\n';
   auto f = [](int x) { return !x ? 0 : (x == 1 ? -1 : x - 1); };
-  rep(i, 2, sz(p)) cout << f(p[i]) << ' ' << f(t.t[i].link) << '\n';
+  rep(i, 2, sz(p)) cout << f(p[i]) << ' ' << f(t.link[i]) << '\n';
   rep(i, 0, sz(s)) cout << f(v[i]) << " \n"[i == sz(s) - 1];
 }
